9b1d8e12287e8636c903cc91cc1a70a41fb2984e
CarlosScarborough45 <cscarborough83@gmail.com>
2026-06-16 12:24:37 +1200

    105Module

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include "raylib.h"
 
 int main()
 {
+    InitWindow(800, 950, "Eat and Greet");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+        DrawText("Welcome to Eat and Greet", 50, 50, 50, BLACK);
+
+        EndDrawing();
+    }
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
