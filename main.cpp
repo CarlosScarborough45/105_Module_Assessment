dcfb382518c4070b7f91c735bd903cdc8d4823bc
CarlosScarborough45 <cscarborough83@gmail.com>
2026-06-26 16:26:36 +1200

    Update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "+==================================================+" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "|             Welcome to Eat && Treats             |" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "+==================================================+" << std::endl;
+
+
+    std::cout << "+==================================================+" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "|  1. Login / Registration | 2. Tables / Orders    |" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "|  3. Menu                 |  4. Billings          |" << std::endl;
+    std::cout << "|                                                  |" << std::endl;
+    std::cout << "+==================================================+" << std::endl;
+
     return 0;
 }
