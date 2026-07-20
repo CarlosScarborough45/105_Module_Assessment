0d582c9c35284e38ec45cea7166aa88efe3fbad5
Carlos Anthony Scarborough <932400098@yoobeestudent.ac.nz>
2026-07-20 12:09:33 +1200

    W.I.P Added small changes

--- Staff.h
+++ Staff.h
@@ -15,7 +15,12 @@ class WaitStaff : public Staff {
     public:
         void rolesdashboard() override {
             std::cout << "WaitStaff Display\n";
-
+            std::cout << "1. Current Menu Display\n";
+            std::cout << "2. View all Tables\n";
+            std::cout << "3. Create new orders\n";
+            std::cout << "4. View order Status (Pending / InProgress / Ready / Served)\n";
+            std::cout << "5. Mark Order as Served\n";
+            std::cout << "6. Return to menu\n";
         }
 };
 
@@ -23,7 +28,9 @@ class Kitchen : public Staff {
     public:
         void rolesdashboard() override {
             std::cout << "KitchenStaff Display\n";
-
+            std::cout << "1. View Incoming Orders (Pending / InProgress)\n";
+            std::cout << "2. Change Status (Pending / In Progress / Ready)\n";
+            std::cout << "3. Return to menu\n";
         }
 };
 
@@ -31,7 +38,11 @@ class Host : public Staff  {
     public:
             void rolesdashboard() override{
                 std::cout << "This is Hosts Display\n";
-
+                std::cout << "1. View live tables (Available / Occupied / Reserved)\n";
+                std::cout << "2. Assign Guest to Table\n";
+                std::cout << "3. Create / Edit / Cancel reservations\n";
+                std::cout << "4. Mark Table as Available\n";
+                std::cout << "5. Return to Menu\n";
             }
 };
 
