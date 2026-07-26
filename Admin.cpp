a5fdfdb7c01fd68f5ee646b812b4fd8cd4591d4a
Carlos Anthony Scarborough <932400098@yoobeestudent.ac.nz>
2026-07-27 10:24:22 +1200

    W.I.P more design Changes for Admin

--- Admin.cpp
+++ Admin.cpp
@@ -1,1 +1,24 @@
 #include "Admin.h"
+#include <iostream>
+
+void Admindisplay() {
+
+    int Admin;
+    std::cout << "1. Create/edit/delete staff accounts\n";
+    std::cout << "2. Full menu control\n";
+    std::cout << "3. Full table configuration\n";
+    std::cout << "4. View all reports, all history\n";
+    std::cout << "5. Override/cancel any order or reservation\n"
+    std::cout << "6. Return to menu\n";
+    std::cin >> Admin;
+
+    switch (Admin) {
+        case 1: {break;}
+        case 2: {break;}
+        case 3: {break;}
+        case 4: {break;}
+        case 5: {break;}
+        case 6: {break;}
+        default: {std::cout << "Must choose between 1 - 6" << std::endl; break;}
+    }
+}
--- Admin.h
+++ Admin.h
@@ -2,11 +2,13 @@
 #define BACKEND_00P_ADMIN_H
 #include "Users.h"
 
+void Admindisplay();
 
 class Admin : public Users {
 public:
     void rolesdashboard() {
         std::cout << "Welcome to Admin Dashboard" << std::endl;
+        Admindisplay();
     }
 };
 
